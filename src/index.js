8782c87e1c7f4186a5be272179fd64acda5ca961
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-20 20:28:56 -0300

    Create the store with initialState

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createStore from 'unistore';
+import { Provider } from 'unistore/react';
+import { initialState } from './reducer';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));

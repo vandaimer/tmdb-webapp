3fd3c674a6cdabfd2455f1533a599be9221f06ff
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-21 01:48:04 -0300

    Add title on the page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/styles';
 import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
 import MovieList from '../MovieList';
 import Pagination from '../Pagination';
 import Search from '../Search';
@@ -9,6 +10,7 @@ function App(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
+      <Typography variant="h1" component="h2">Upcoming Movies</Typography>
       <Search />
       <MovieList />
       <Pagination />

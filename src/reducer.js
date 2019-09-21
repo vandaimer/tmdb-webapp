cf3c20f5f457c5e979305dce714d18fdabb0c3a0
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-20 21:18:07 -0300

    Implement getUpcomingMovies on reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 
 const actions = store => ({});
 const apiURL = 'http://localhost:8000';
@@ -5,6 +6,21 @@ const baseURL = `${apiURL}/api/v1/movie`;
 
 const initialState = {
   movieList: [],
+};
+
+const getUpcomingMovies = async state => {
+  const uri = `${baseURL}/upcoming`;
+
+  try {
+    const {
+      data: { items: movieList },
+    } = await axios.get(uri);
+
+    return { movieList };
+  } catch (e) {
+    const { movieList } = state;
+    return { movieList };
+  }
 };
 
 const props = ['movieList'];

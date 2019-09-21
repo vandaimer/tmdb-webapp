11687144227ecfdd80f0151245f14df8df279ca9
Luiz Filipe <moresco.luiz@gmail.com>
2019-09-20 21:17:29 -0300

    Add movieList item to props on reducer

--- src/reducer.js
+++ src/reducer.js
@@ -3,9 +3,10 @@ const actions = store => ({});
 const apiURL = 'http://localhost:8000';
 const baseURL = `${apiURL}/api/v1/movie`;
 
-const props = [];
 const initialState = {
   movieList: [],
 };
+
+const props = ['movieList'];
 
 export { initialState, props, actions };
